5b5eb8f2455040e77fd7e5da6fdaf4bbc54e5c6e
xiaofan0208 <xiaofan0208@126.com>
2020-07-14 23:20:38 +0800

    修改弹窗提示

---
+++ static/js/my-sweetalert2.js
@@ -0,0 +1,45 @@
+// SweetAlert2  弹窗
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
+// 成功弹窗
+function swalDefaultSuccess(title) {
+    Toast.fire({
+        icon: 'success',
+        title: title
+    })
+}
+// 错误弹窗提示
+function swalDefaultError(title) {
+    Toast.fire({
+        icon: 'error',
+        title: title
+    })
+}
+
+// 消息弹窗
+function swalDefaultInfo(title) {
+    Toast.fire({
+        icon: 'info',
+        title: title
+    })
+}
+
+// 警告
+function swalDefaultWarning(title) {
+    Toast.fire({
+        icon: 'warning',
+        title: title
+    })
+}
+
+// 问题弹窗
+function swalDefaultQuestion(title) {
+    Toast.fire({
+        icon: 'question',
+        title: title
+    })
+}
