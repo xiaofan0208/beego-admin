24e1b7aad64a1a7d6681307aab27daa347702d66
xiaofan0208 <xiaofan0208@126.com>
2020-07-18 22:53:48 +0800

    完善编辑页面

--- static/js/my-sweetalert2.js
+++ static/js/my-sweetalert2.js
@@ -9,43 +9,70 @@ const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 3000
+    timer: 2000
 });
 // 成功弹窗
-function swalDefaultSuccess(title) {
+function swalDefaultSuccess(title , callback) {
     Toast.fire({
         icon: 'success',
-        title: title
+        title: title,
+       
+        onClose : function() {
+            if(callback != undefined) {
+                callback()
+            }
+        }
     })
 }
 // 错误弹窗提示
-function swalDefaultError(title) {
+function swalDefaultError(title, callback) {
     Toast.fire({
         icon: 'error',
-        title: title
+        title: title,
+        background : "#d33",
+        onClose : function() {
+            if(callback != undefined) {
+                callback()
+            }
+        }
     })
 }
 
 // 消息弹窗
-function swalDefaultInfo(title) {
+function swalDefaultInfo(title, callback) {
     Toast.fire({
         icon: 'info',
-        title: title
+        title: title,
+        onClose : function() {
+            if(callback != undefined) {
+                callback()
+            }
+        }
     })
 }
 
 // 警告
-function swalDefaultWarning(title) {
+function swalDefaultWarning(title, callback) {
     Toast.fire({
         icon: 'warning',
-        title: title
+        title: title,
+        onClose : function() {
+            if(callback != undefined) {
+                callback()
+            }
+        }
     })
 }
 
 // 问题弹窗
-function swalDefaultQuestion(title) {
+function swalDefaultQuestion(title, callback) {
     Toast.fire({
         icon: 'question',
-        title: title
+        title: title,
+        onClose : function() {
+            if(callback != undefined) {
+                callback()
+            }
+        }
     })
 }
