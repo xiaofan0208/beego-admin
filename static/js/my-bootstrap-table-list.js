3dd0fbe2a0ba0325292cf8e48c0c831a0382b6b5
xiaofan0208 <xiaofan0208@126.com>
2020-08-11 22:32:06 +0800

    添加角色管理

--- static/js/my-bootstrap-table-list.js
+++ static/js/my-bootstrap-table-list.js
@@ -27,7 +27,7 @@
                 showColumns: true, //是否显示所有的列
                 showRefresh: true, //是否显示刷新按钮
                 minimumCountColumns: 2, //最少允许的列数
-                clickToSelect: true, //是否启用点击选中行
+                clickToSelect: false, //是否启用点击选中行
                 //height: 500, //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
                 uniqueId: "ID", //每一行的唯一标识，一般为主键列
                 showToggle: true, //是否显示详细视图和列表视图的切换按钮
@@ -55,7 +55,7 @@
         // 获取搜索参数
         function getSearchParam() {
             var search = {}
-            $("#formSearch .search").each(function () {
+            $("#formSearch .filter-search").each(function () {
                 var key = $(this).attr("id")
                 var value = $.trim($(this).val())
                 if (key != undefined && value != "") {
